29f7b7200bbc3d6e1cc00cb58ca9f157eb25ac6f
Данила Дёмин <danilakemenov312@gmail.com>
2022-12-07 12:05:49 +0300

    правки

--- HaidressersApp/View/Pages/Record.xaml.cs
+++ HaidressersApp/View/Pages/Record.xaml.cs
@@ -4,6 +4,7 @@ using HaidressersApp.View.Windows;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Lifetime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -28,16 +29,22 @@ namespace HaidressersApp.View.Pages
         {
             InitializeComponent();
             record.Add(new RecordClass("2022/10/11", "Каримова", "Милена", "admin"));
+            record.Add(new RecordClass("2022/10/11", "Демин", "Данила", "admin"));
+            record.Add(new RecordClass("2022/10/11", "Карпова", "Милена", "barber"));
+            record.Add(new RecordClass("2022/10/11", "Каримова", "Милена", "barber"));
+            record.Add(new RecordClass("2022/10/11", "Маслова", "Милена", "client"));
+            record.Add(new RecordClass("2022/10/11", "Макаркин", "Егор", "client"));
+            LoadUser(record);
         }
 
         private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
-            CustomersList.ItemsSource = ConnectClass.entities.Uchet.ToList();
+           // CustomersList.ItemsSource = ConnectClass.entities.Uchet.ToList();
         }
 
         private void CustomersList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            Uchet journal = (Uchet)CustomersList.SelectedItem;
+            //Uchet journal = (Uchet)CustomersList.SelectedItem;
         }
 
         private void AddBtn_Click(object sender, RoutedEventArgs e)
@@ -48,30 +55,36 @@ namespace HaidressersApp.View.Pages
              
         private void Delettn_Click(object sender, RoutedEventArgs e)
         {
-
+            // Removes the first item in the list.
+            CustomersList.Items.RemoveAt(0);
+            MessageBox.Show("Запись удалена " + "!", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
         }
-        public void LoadUser(List<User> _user)
+        public void LoadUser(List<RecordClass> _record)
         {
             CustomersList.Items.Clear(); // очищаем лист с элементами
 
-            for (int i = 0; i < _user.Count; i++) // перебираем элементы
+            for (int i = 0; i < _record.Count; i++) // перебираем элементы
             {
-                CustomersList.Items.Add(_user[i]); // добавляем элементы в ListBox
+                CustomersList.Items.Add(_record[i]); // добавляем элементы в ListBox
             }
         }
-        private void ActiveFilter(object sender, RoutedEventArgs e)
-        {
-            List<User> newUsers = new List<User>();
-            //newUsers = user;
-
-            //if (genderFilter.SelectedIndex == 0)
-            //    newUsers = user.FindAll(x => x.gender == "M");
-            //else
-            //    newUsers = user.FindAll(x => x.gender == "F");
-            //newUsers = newUsers.FindAll(x => x.name.Contains(nameFilter.Text));
-            //LoadUser(newUsers);
-
-        }
         
+
+        private void ActiveFilter_Click(object sender, RoutedEventArgs e)
+        {
+            List<RecordClass> newRecord = new List<RecordClass>();
+            newRecord = record;
+
+            if (txtActivityName.SelectedIndex == 0)
+                newRecord = record.FindAll(x => x.Title == "admin");
+            else
+                newRecord = record.FindAll(x => x.Title == "client");
+            if (txtActivityName.SelectedIndex == 2)
+                newRecord = record.FindAll(x => x.Title == "barber");
+
+            newRecord = newRecord.FindAll(x => x.Title.Contains(SearchTextBox.Text));
+
+            LoadUser(newRecord);
+        }
     }
 }
